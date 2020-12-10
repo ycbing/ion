ffebfd28769331c699eb8561f527ca8cd09fca2a
caibing yan <caibingyan@caibings-MacBook-Pro.local>
2020-12-10 20:45:45 +0800

    update queue

--- 数据结构与算法/队列/Queue.js
+++ 数据结构与算法/队列/Queue.js
@@ -84,3 +84,200 @@ function PriorityQueue() {
     return items.length;
   };
 }
+
+
+
+class Queue {
+  constructor () {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count += 1;
+  }
+
+  dequeue() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount += 1;
+    return result;
+  }
+
+  peek() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {}
+  }
+
+  // 定义 toString 方法，返回队列中的内容
+  toString() {
+    if(this.isEmpty()) {
+      return '';
+    }
+    let objString =  `${this.items[this.lowestCount]}`;
+    for(let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+class Deque {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  addFront(element) {
+    if(this.isEmpty()) {
+      this.addBack(element);
+    } else if (this.lowestCount > 0) {
+      this.lowestCount += 1;
+      this.items[this.lowestCount] = element;
+    } else {
+      for(let i = this.count; i > 0; i--) {
+        this.items[i] = this.items[i-1];
+      }
+      this.count += 1;
+      this.items[0] = element;
+    }
+  }
+
+  addBack(element) {
+    this.items[this.count] = element;
+    this.count += 1;
+  }
+
+  removeFront() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount += 1;
+    return result;
+  }
+
+  removeBack() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    this.count += 1;
+    const result = this.items[this.count];
+    delete this.items[this.count];
+    return result;
+  }
+
+  peekFront() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  peekBack() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.count - 1];
+  }
+
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  clear() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  toString() {
+    if(this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for(let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString}, ${this.items[i]}`;
+    }
+    return objString;
+  }
+}
+
+
+
+const hotPotato = (elementsList, num) => {
+  const queue = new Queue();
+  const elimitatedList = [];
+
+  for(let i = 0, l = elementsList.length; i < l; i++) {
+    queue.enqueue(elementsList[i]);
+  }
+
+  while (queue.size() > 1) {
+    for(let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+    elimitatedList.push(queue.dequeue());
+  }
+
+  return {
+    eliminated: elementsList,
+    winner: queue.dequeue()
+  }
+}
+
+
+const palindromeChecker = (aString) => {
+  if(
+    aString === undefined ||
+    aString === null ||
+    (aString !== null && aString.length === 0)
+  ) {
+    return false;
+  }
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split(' ').join('');
+  let firstChar;
+  let lastChar;
+
+  for(let i = 0,l = lowerString.length; i < l; i++) {
+    deque.addBack(lowerString.charAt(i));
+  }
+
+  while (deque.size() > 1) {
+    firstChar = deque.removeFront();
+    lastChar = deque.removeBack();
+    if(firstChar !== lastChar) {
+      return false;
+    }
+  }
+
+  return true;
+} 
